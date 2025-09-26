3a7fb88cb9b51dc1df38a910018f5c25fb92d3e9
TioSniperxD <creaducuenta6@hotmail.com>
2025-09-25 23:40:19 -0500

    Commit en Rama

---
+++ script.js
@@ -0,0 +1,24 @@
+// Declaración de variables
+let nombre = "Emerson";
+const edad = 20;
+var pais = "Perú";
+
+// Imprimir en la consola
+console.log("Hola desde script.js");
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("País:", pais);
+
+// Función simple sin parámetros
+function saludar() {
+  console.log("¡Hola mundo!");
+}
+
+// Función que recibe un valor
+function mostrarDato(dato) {
+  console.log("El dato es:", dato);
+}
+
+// Llamadas a las funciones
+saludar();
+mostrarDato(42);
